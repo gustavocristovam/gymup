7713b387060aa9763156b134e0eac84669b35ce3
Gustavo Cristovam <71038617+gustavocristovam@users.noreply.github.com>
2023-12-24 18:12:13 -0300

    Tirando o PHP

--- request/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('HOST', 'localhost');
-define('USUARIO', 'root');
-define('SENHA', '8j3L1PCmnbAQa4UTkhg8');
-define('DB', 'data_gym');
-
-
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
-?>
