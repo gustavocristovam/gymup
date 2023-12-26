bd06f445e22f3dbdfcc416ce670d0de31697e489
Gustavo Cristovam <71038617+gustavocristovam@users.noreply.github.com>
2023-12-26 17:26:51 -0300

    fatorial

--- script/script.js
+++ script/script.js
@@ -14,12 +14,40 @@ function calculo() {
         img.setAttribute('id', 'foto')
         if (idade_atual < 18) {
             resultado.innerHTML += '<p>Você é de <strong>MENOR!</strong></p>'
-            img.setAttribute('src','../imagems/probiido.png')
+            img.setAttribute('src', '../imagems/probiido.png')
             resultado.appendChild(img)
         } else {
             resultado.innerHTML += '<p>Você é de <strong>MAIOR!</strong></p>'
-            img.setAttribute('src','../imagems/carteira.png')
+            img.setAttribute('src', '../imagems/carteira.png')
             resultado.appendChild(img)
         }
+    }
+}
+
+function contar() {
+    let resultado = document.getElementById('saida')
+    resultado.innerHTML = ''
+    let start = Number(window.document.getElementById('startid').value)
+    let end = Number(window.document.getElementById('endid').value)
+    let jump = Number(window.document.getElementById('jumpid').value)
+
+    if (window.document.getElementById('startid').value.length == 0 || end == 0) {
+        window.alert('INSIRA OS VALORES EM INICIO E FIM!')
+
+    } else {
+        if (jump == 0) {
+            window.alert('VOCÊ NAO COLOCOU O DIGITO EM SALTAR A CADA. Iremos definir o valor 1!')
+            jump = 1
+        }
+
+        for (let number = start; number <= end; number += jump) {
+
+            resultado.innerHTML += `<span>&#128511; ${number}</span>`
+            
+
+           
+
+        }
+        resultado.innerHTML += `<span>&#128507;</span>`
     }
 }
